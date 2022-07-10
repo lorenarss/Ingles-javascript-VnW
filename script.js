d309e91c53845b3d7e3a4444167380a3437de4d4
Lorena Silva <lorena.silva@codeminer42.com>
2022-07-10 08:59:24 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,54 @@
+let comedy = [];
+let action = [];
+let fantasy = [];
+
+let filme1 = new Object();
+filme1.title = 'Back to the Future';
+filme1.director = 'Robert Zemeckis';
+filme1.year = 1985;
+
+let filme2 = new Object();
+filme2.title = 'Beetlejuice';
+filme2.director = 'Tim Burton';
+filme2.year = 1988;
+
+let filme3 = new Object();
+filme3.title = 'Clueless';
+filme3.director = ' Amy Heckerling';
+filme3.year = 1995;
+
+comedy.push(filme1, filme2, filme3);
+
+let filme4 = new Object();
+filme4.title = 'The terminator';
+filme4.director = 'James Cameron';
+filme4.year = 1984;
+
+let filme5 = new Object();
+filme5.title = 'batman';
+filme5.director = 'Matt Reeves';
+filme5.year = 2022;
+
+let filme6 = new Object();
+filme6.title = 'Doctor Strange';
+filme6.director = 'Scott Derrickson';
+filme6.year =  2016;
+
+action.push(filme4, filme5, filme6);
+
+let filme7 = new Object();
+filme7.title = 'Harry Potter and the Half-Blood Prince';
+filme7.director = ' David Yates';
+filme7.year =  2009;
+
+let filme8 = new Object();
+filme8.title = 'Harry Potter and the Goblet of Fire ';
+filme8.director = ' Mike Newell';
+filme8.year =  2005;
+
+let filme9 = new Object();
+filme9.title = 'Harry Potter and the Prisoner of Azkaban';
+filme9.director = ' Alfonso Cuarón';
+filme9.year =  2004;
+
+fantasy.push(filme7, filme8, filme9);
